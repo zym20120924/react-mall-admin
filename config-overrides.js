b99fd0b4322c8c9ba8c370a8633655b37c30e73c
汤国斌 <1002501259@qq.com>
2018-05-28 20:48:26 +0800

    初次提交

---
+++ config-overrides.js
@@ -0,0 +1,8 @@
+const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
+
+module.exports = function override(config, env) {
+    config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
+    config = rewireLess(config, env);
+    return config;
+};
